d55ca51dc71d2f6244df77ca0a06d600f6643fcc
Papon <papon.s@lawyerplusgroup.com>
2023-03-30 14:42:39 +0700

    [The Complete Android 12 & Kotlin Development Masterclass][Section 10: 7 Minute Workout App] 145. Using CountDownTimers To Display The Seconds remaining (Reviewed by Papon S.)

--- app/src/main/java/com/example/a7minworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minworkout/ExerciseActivity.kt
@@ -3,12 +3,16 @@ package com.example.a7minworkout
 import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import com.example.a7minworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private var binding: ActivityExerciseBinding? = null
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,49 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressedDispatcher.onBackPressed()
         }
 
+        setUpRestView()
+
+    }
+
+    private fun setRestProgressbar() {
+        binding?.progressbar?.progress = restProgress
+        restTimer = object: CountDownTimer(10000,1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                val progressTimerValue: Int = 10 - restProgress
+                binding?.progressbar?.progress = progressTimerValue
+                binding?.tvTimer?.text = progressTimerValue.toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise.",
+                     Toast.LENGTH_LONG
+                ).show()
+            }
+
+        }.start()
+
+    }
+
+    private fun setUpRestView() {
+        stopTimer()
+        setRestProgressbar()
+    }
+
+    private fun stopTimer() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        stopTimer()
+        binding = null
     }
 
 }
